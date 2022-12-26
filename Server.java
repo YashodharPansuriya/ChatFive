333073d8adb073c417bf1b4ffa3d222e0aa261da
YashodharPansuriya <yashodhar.pansuriya@gmail.com>
2022-12-26 12:52:38 +0100

    Implement mentod that read the client message

--- Server.java
+++ Server.java
@@ -1,5 +1,60 @@
+import java.io.*;
+import java.net.*;
+
 class Server {
+    ServerSocket serverSocket;
+    Socket socket;
+    BufferedReader br;
+    PrintWriter pout;
+
+    // Constructor
+    public Server() {
+        try {
+            serverSocket = new ServerSocket(7777);
+            System.out.println("Server is ready to accept connections");
+            System.out.println("Waiting for connections...");
+
+            // Accept connections
+            socket = serverSocket.accept();
+
+            // Read and write messages
+            br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            pout = new PrintWriter(socket.getOutputStream());
+
+            // Function for Read and write messages
+            startReading();
+            startWriting();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Read the message from the client (socket)
+    public void startReading() throws IOException {
+        // Thread -- Read the message from the client
+        Runnable r1 = () -> {
+            System.out.println("Read the  message from the client...");
+
+            // Read the infinit message from the client
+            while (true) {
+                try {
+                    String msg = br.readLine();
+                    if(msg.equals("exit")){
+                        System.out.println("Client terminated the chat");
+                        break;
+                    }
+
+                    System.out.println("Client: " + msg);	
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+    }
+
     public static void main(String[] args) {
         System.out.println("Server is starting...");
+        new Server();
     }
 }
